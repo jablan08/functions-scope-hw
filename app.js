e8afa611d32f7831ae66b7682bf43fd2953a91e1
Jablan <joshuaablan@gmail.com>
2019-03-21 16:43:13 -0700

    complete verbal questions

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // 1. Verbal questions
 // What is the difference between a parameter and an argument?
+    // The argument is the input, the parameter is how the input is represented in the function.
 // Within a function, what is the difference between return and console.log?
+    // console.log is will print out whatever is specified in the (). Return will return a value of a function to pass data.
 // What are the implications of the ability of a function to return a value?
+    // Return ends the function.
 // 
 
 
