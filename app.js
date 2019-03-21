51d5192586b6bebe14ccdcec193a218ef59a8034
Jablan <joshuaablan@gmail.com>
2019-03-21 16:54:33 -0700

    Finished Palindrome

--- app.js
+++ app.js
@@ -7,4 +7,23 @@
     // Return ends the function.
 // 
 
+// 2. Palindrome again.
+// Write a function checkPalindrome that accepts a single argument, a string. 
+// Yes, you've done it before, but do it again. Later in this assignment we're 
+// gonna beef up our palindrome function some. See if you can do it without 
+// looking back at your previous answer. The function should return true 
+// if the string is a palindrome, false if not. Make sure your function will 
+// give the correct answer for words with capital letters.
 
+const checkPalindrome = (str) => {
+    let strRev = str.split("").reverse().join("")
+    // console.log(strRev);
+    if (strRev.toLowerCase() === str.toLowerCase()) {
+        console.log("This is Palindrome.");
+    } else {
+        console.log("Not a Palindrome.");
+    } 
+    // split reverse join
+}
+
+checkPalindrome("Hello");
