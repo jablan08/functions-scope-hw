d6f2b71861803766f045bef5c9514d0ca23d2c61
Jablan <joshuaablan@gmail.com>
2019-03-21 16:09:14 -0700

    initial commit created files

---
+++ app.js
@@ -0,0 +1,7 @@
+// 1. Verbal questions
+// What is the difference between a parameter and an argument?
+// Within a function, what is the difference between return and console.log?
+// What are the implications of the ability of a function to return a value?
+// 
+
+
